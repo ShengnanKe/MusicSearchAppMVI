8c95f169a9ee86b1571040f3d041acfb8a6d15c5
KknanxX <72762570+KknanxX@users.noreply.github.com>
2024-07-06 18:42:20 -0700

    revoke test

--- MusicSearchAppMVI/Views/MusicSearchView.swift
+++ MusicSearchAppMVI/Views/MusicSearchView.swift
@@ -12,7 +12,6 @@ struct MusicSearchView: View {
     @State private var isSearching = false
     
     init() {
-        let a  = 1
         let model = MusicSearchModel()
         let intent = MusicSearchIntent(model: model)
         _container = StateObject(wrappedValue: MVIContainer(intent: intent, model: model, modelChangePublisher: model.objectWillChange))
